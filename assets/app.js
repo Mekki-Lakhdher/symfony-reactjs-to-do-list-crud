654291abbe1c68ee852e910e499d851e3bfe824e
Mekki Lakhdher <mlakhdher@gmail.com>
2021-02-24 16:01:33 +0100

    created a context and rendered the app onto the template

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,18 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import TodoTable from "./components/TodoTable";
+import TodoContextProvider from "./contexts/TodoContext";
 
-console.log('Hello app !');
+class App extends Component {
+    render() {
+        return (
+            <TodoContextProvider>
+                <TodoTable/>
+            </TodoContextProvider>
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+    )
+        ;
+    }
+}
 
-// start the Stimulus application
-import './bootstrap';
+ReactDom.render(<App/>,document.getElementById('root'));
